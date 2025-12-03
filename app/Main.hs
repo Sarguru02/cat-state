d6d9de76e7c22ad5c6afbc8cdc9059556e58957c
Sarguru02 <psgnhealth@gmail.com>
2025-12-03 14:39:31 +0530

    Make everything single file again

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,58 @@
 module Main where
 
 import qualified Data.Text as T
-import qualified CatState.App as App
+import           Data.OpenFeature.SuperpositionProvider (defaultProviderOptions, SuperpositionProviderOptions(..), RefreshOptions(..), newSuperpositionProvider, SuperpositionProvider)
+import qualified Network.URI as Net
+import qualified Data.OpenFeature.Api as OpenFeature
+import qualified Data.OpenFeature.Client as OpenFeature
+import qualified Data.OpenFeature.EvaluationContext as EC
+import           Data.Aeson (toJSON)
+
 
 main :: IO ()
 main = do
-  eClient <- App.getClient
+  eClient <- getClient
   case eClient of
     Left err -> putStrLn $ "Error occurrred: " <> T.unpack err
-    Right client -> App.useClient client
+    Right client -> useClient client
+
+getProvider :: String -> Text -> Text -> Int -> IO (Either Text SuperpositionProvider)
+getProvider uri orgId workspaceId pollTime = do
+  case Net.parseURI uri of
+    Nothing -> pure $ Left "URI IS NOT PARSED"
+    Just uriVal -> do
+      let options = defaultProviderOptions
+                  { orgId = orgId
+                  , workspaceId = workspaceId
+                  , endpoint = uriVal
+                  , refreshOptions = Poll pollTime
+                  }
+      newSuperpositionProvider options
+
+getClient :: IO (Either Text OpenFeature.Client)
+getClient = do
+  eProvider <- getProvider "http://localhost:8080" "localorg" "dev" 5
+  case eProvider of
+    Left err -> pure $ Left $ "Error when getting provider: " <> err
+    Right provider -> do
+      _ <- OpenFeature.setNamedProvider "CAC" provider
+      _ <- putStrLn $ "Done setting up provider with name"
+      client <- OpenFeature.createNamedClient "OF_CLIENT"
+      pure $ Right client
+
+useClient :: OpenFeature.Client -> IO ()
+useClient client' = do
+  let context = getContext
+  let client = OpenFeature.setClientContext context client'
+  evalResult <- OpenFeature.getIntValue client "k1" ( Just context )
+  putStrLn $ show evalResult
+
+
+getContext :: EC.EvaluationContext
+getContext = 
+  EC.defaultContext 
+    & EC.withCustomField "A" (toJSON ("Te"::String))
+    & EC.withCustomField "B" (toJSON ("Re" :: String))
+    & EC.withCustomField "C" (toJSON ("CT"::String))
+    & EC.withCustomField "SM1" (toJSON ("CT1"::String))
+    & EC.withCustomField "SM2" (toJSON ("CT2"::String))
