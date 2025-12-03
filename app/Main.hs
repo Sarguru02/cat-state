58e8bd6f7f483dcf7a079e27d37c249486ae51b0
Sarguru02 <psgnhealth@gmail.com>
2025-12-03 14:36:44 +0530

    Setup and getting config with some dimensions

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,11 @@
 module Main where
 
-import qualified Network.HTTP.Client.TLS as HttpTLS
 import qualified Data.Text as T
 import qualified CatState.App as App
 
 main :: IO ()
 main = do
-  manager <- HttpTLS.newTlsManagerWith HttpTLS.tlsManagerSettings
-  eClient <- App.createClient manager
+  eClient <- App.getClient
   case eClient of
-    Left err -> putStrLn $ "Error occurred when creating client: " <> T.unpack err
+    Left err -> putStrLn $ "Error occurrred: " <> T.unpack err
     Right client -> App.useClient client
--- lib/CatState/App.hs
+++ lib/CatState/App.hs
@@ -1,8 +1,49 @@
 module CatState.App where
 
+import           Data.OpenFeature.SuperpositionProvider (defaultProviderOptions, SuperpositionProviderOptions(..), RefreshOptions(..), newSuperpositionProvider, SuperpositionProvider)
+import qualified Network.URI as Net
+import qualified Data.OpenFeature.Api as OpenFeature
+import qualified Data.OpenFeature.Client as OpenFeature
+import qualified Data.OpenFeature.EvaluationContext as EC
+import           Data.Aeson (toJSON)
+
+getProvider :: String -> Text -> Text -> Int -> IO (Either Text SuperpositionProvider)
+getProvider uri orgId workspaceId pollTime = do
+  case Net.parseURI uri of
+    Nothing -> pure $ Left "URI IS NOT PARSED"
+    Just uriVal -> do
+      let options = defaultProviderOptions
+                  { orgId = orgId
+                  , workspaceId = workspaceId
+                  , endpoint = uriVal
+                  , refreshOptions = Poll pollTime
+                  }
+      newSuperpositionProvider options
+
+getClient :: IO (Either Text OpenFeature.Client)
+getClient = do
+  eProvider <- getProvider "http://localhost:8080" "localorg" "dev" 5
+  case eProvider of
+    Left err -> pure $ Left $ "Error when getting provider: " <> err
+    Right provider -> do
+      _ <- OpenFeature.setNamedProvider "CAC" provider
+      _ <- putStrLn $ "Done setting up provider with name"
+      client <- OpenFeature.createNamedClient "OF_CLIENT"
+      pure $ Right client
+
+useClient :: OpenFeature.Client -> IO ()
+useClient client' = do
+  let context = getContext
+  let client = OpenFeature.setClientContext context client'
+  evalResult <- OpenFeature.getIntValue client "k1" ( Just context )
+  putStrLn $ show evalResult
 
 
-createClient = undefined
-
-
-useClient = undefined
+getContext :: EC.EvaluationContext
+getContext = 
+  EC.defaultContext 
+    & EC.withCustomField "A" (toJSON ("Te"::String))
+    & EC.withCustomField "B" (toJSON ("Re" :: String))
+    & EC.withCustomField "C" (toJSON ("CT"::String))
+    & EC.withCustomField "SM1" (toJSON ("CT1"::String))
+    & EC.withCustomField "SM2" (toJSON ("CT2"::String))
